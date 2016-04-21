113911d98d56aafa850285ffbc2516ee169bb482
roy <roy@keyroy-ws>
2016-04-21 10:29:51 +0800

    增加了 根据 PSD 大小自动拉伸图片的 功能 默认关闭

--- gdx-psd-reflect-framework/src/psd/reflect/PsdImage.java
+++ gdx-psd-reflect-framework/src/psd/reflect/PsdImage.java
@@ -3,6 +3,7 @@ package psd.reflect;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -16,6 +17,10 @@ import psd.framework.PsdReflectUtil;
  * @author roy
  */
 public class PsdImage extends Image {
+	// 自动拉伸到 PSD设定大小 全局
+	private static boolean defAutoScale = false;
+	// 自动拉伸到 PSD设定大小
+	protected boolean autoScale = defAutoScale;
 	// 图片源
 	protected final psd.Pic psdPic;
 
@@ -32,7 +37,7 @@ public class PsdImage extends Image {
 	}
 
 	public PsdImage(psd.PsdFile psdFile, psd.Pic pic, AssetManager assetManager) {
-		super(getTexture(psdFile, pic, assetManager));
+		super(getTexture(psdFile, pic, assetManager, defAutoScale));
 		this.psdPic = pic;
 	}
 
@@ -43,24 +48,46 @@ public class PsdImage extends Image {
 
 	// 读取图片
 	protected static final TextureRegion getTexture(psd.PsdFile psdFile, psd.Pic pic,
-			AssetManager assetManager) {
+			AssetManager assetManager, boolean scaleToPicSize) {
 		if (assetManager == null) {
 			assetManager = PsdGroup.getAssetManager();
 		}
+		TextureRegion region = null;
+
 		if (psdFile == null || psdFile.atlas == null) {
 			Texture texture = PsdReflectUtil.load(assetManager, pic.textureName, Texture.class);
 			texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-			return new TextureRegion(texture);
+			region = new TextureRegion(texture);
 		} else {
 			TextureAtlas textureAtlas = PsdReflectUtil.load(assetManager, psdFile.getAtlasPath(),
 					TextureAtlas.class);
 			AtlasRegion atlasRegion = textureAtlas.findRegion(pic.textureName);
 			if (atlasRegion != null) {
 				atlasRegion.getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
-				return atlasRegion;
+				region = new TextureRegion(atlasRegion);
 			}
 		}
 
-		return null;
+		// 拉伸图片
+		if (region != null && scaleToPicSize && pic.width > 0 && pic.height > 0) {
+			float scaleX = region.getRegionWidth() / pic.width;
+			float scaleY = region.getRegionHeight() / pic.height;
+			Sprite sprite = new Sprite(region);
+			sprite.setScale(scaleX, scaleY);
+			//
+			region = sprite;
+		}
+
+		return region;
+	}
+
+	// 自动拉伸到 PSD设定大小 全局
+	public static void setDefAutoScale(boolean defAutoScale) {
+		PsdImage.defAutoScale = defAutoScale;
+	}
+
+	// 自动拉伸到 PSD设定大小
+	public void setAutoScale(boolean autoScale) {
+		this.autoScale = autoScale;
 	}
 }
